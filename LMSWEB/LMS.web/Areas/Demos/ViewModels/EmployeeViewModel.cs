0498689b4b79eb31d356829cb26cc9c8245306c6
ashutoshsinghshrinet <ashutoshsinghshrinet0@gmail.com>
2022-08-02 15:17:29 +0530

    LMSWEB

---
+++ LMSWEB/LMS.web/Areas/Demos/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace LMS.web.Areas.Demos.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        [Required(ErrorMessage ="This is a required feild")]
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage ="This {0} is required feild")]
+        
+        public string EmployeeName { get; set; }
+        [Required]
+        public DateTime DateofBirth { get; set; }
+        [Range(minimum:0,maximum:2000000, ErrorMessage ="{0} has to be beetween {1} and {2}")]
+        public decimal Salary { get; set; }
+        public bool IsEnabled { get; set; }
+
+    }
+}
---
+++ Practice/E-Commerce/Areas/Demos/Controllers/FlipkartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace E_Commerce.Areas.Demos.Controllers
+{
+    public class FlipkartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
